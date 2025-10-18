9580be21644e15ebb0a2dd89163244f9836d10aa
TaniaFedortsi <tania.fdrts@gmail.com>
2025-10-18 17:05:19 +0300

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,8 @@ export const App = () => {
   const [sortedGoods, setSortedGoods] = useState(goodsFromServer);
   const [isReversed, setIsReversed] = useState(false);
   const [sortType, setSortType] = useState(null);
-  const isModified = JSON.stringify(sortedGoods) !== JSON.stringify(goodsFromServer);
+  const isModified =
+    JSON.stringify(sortedGoods) !== JSON.stringify(goodsFromServer);
 
   const sortGoods = type => {
     const goods = [...goodsFromServer];
@@ -82,11 +83,7 @@ export const App = () => {
         </button>
 
         {isModified && (
-          <button
-            type="button"
-            className="button is-danger"
-            onClick={reset}
-          >
+          <button type="button" className="button is-danger" onClick={reset}>
             Reset
           </button>
         )}
